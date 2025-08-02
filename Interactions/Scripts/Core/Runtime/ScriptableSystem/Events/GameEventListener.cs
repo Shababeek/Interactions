e2afac9cd113b5bc5119cd8f1627458ed3f81d2e
ahmadabobakr <ahmadabobakr@gmail.com>
2025-08-02 13:34:12 +0400

    Game event is no in par feature wise with the step listner

--- Interactions/Scripts/Core/Runtime/ScriptableSystem/Events/GameEventListener.cs
+++ Interactions/Scripts/Core/Runtime/ScriptableSystem/Events/GameEventListener.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UniRx;
 using UnityEngine;
 using UnityEngine.Events;
@@ -6,22 +7,59 @@ using UnityEngine.Events;
 namespace Shababeek.Core
 {
     /// <summary>
-    /// Listener for game events that can be subscribed to and respond to event triggers.
+    /// Listens to game events and raises Unity events accordingly.
+    /// Provides a bridge between the ScriptableObject-based GameEvent system and Unity's event system.
     /// </summary>
+    /// <remarks>
+    /// This component allows you to respond to GameEvent triggers by raising Unity events
+    /// that can be connected to other systems in the scene. It supports multiple GameEvents
+    /// with their associated Unity events.
+    /// </remarks>
+    [AddComponentMenu("Shababeek/Scriptable System/Game Event Listener")]
     public class GameEventListener : MonoBehaviour
     {
-        [Tooltip("The game event to listen to.")]
-        [SerializeField] private GameEvent @event;
-        [Tooltip("Event raised when the game event is triggered.")]
-        [SerializeField] private UnityEvent onRaised;
+        [Tooltip("List of game events to listen to with their associated Unity events.")]
+        [SerializeField] internal List<GameEventWithEvents> gameEventList;
+        
         private CompositeDisposable _disposable;
+
+        /// <summary>
+        /// Gets or sets the list of game events with their associated events.
+        /// </summary>
+        /// <value>The list of GameEventWithEvents containing game events and their Unity events.</value>
+        public List<GameEventWithEvents> GameEventList
+        {
+            get => gameEventList;
+            set => gameEventList = value;
+        }
+
+        /// <summary>
+        /// Adds a new game event to the listener with default event settings.
+        /// </summary>
+        /// <param name="gameEvent">The game event to add to the listener.</param>
+        /// <remarks>
+        /// This method creates a new GameEventWithEvents with the provided game event and adds it to the list.
+        /// The game event will be automatically subscribed to when the component is enabled.
+        /// </remarks>
+        public void AddGameEvent(GameEvent gameEvent)
+        {
+            var gwe = new GameEventWithEvents();
+            gwe.gameEvent = gameEvent;
+            gameEventList ??= new List<GameEventWithEvents>();
+            gameEventList.Add(gwe);
+        }
 
         private void OnEnable()
         {
-            if (@event != null)
+            _disposable = new CompositeDisposable();
+            foreach (var gameEventWithEvents in gameEventList)
             {
-                @event.OnRaised.Do(_ => OnEventRaised()).Subscribe().AddTo(_disposable);
-                _disposable = new CompositeDisposable();
+                if (gameEventWithEvents.gameEvent != null)
+                {
+                    gameEventWithEvents.gameEvent.OnRaised
+                        .Do(_ => OnGameEventRaised(gameEventWithEvents))
+                        .Subscribe().AddTo(_disposable);
+                }
             }
         }
 
@@ -30,10 +68,22 @@ namespace Shababeek.Core
             _disposable?.Dispose();
         }
 
-        private void OnEventRaised()
+        private void OnGameEventRaised(GameEventWithEvents gameEventWithEvents)
         {
-            onRaised?.Invoke();
+            gameEventWithEvents.onGameEventRaised?.Invoke();
         }
 
+        /// <summary>
+        /// Container class that associates a game event with Unity events.
+        /// </summary>
+        [Serializable]
+        public class GameEventWithEvents
+        {
+            [Tooltip("The game event to listen to for triggers.")]
+            public GameEvent gameEvent;
+            
+            [Tooltip("Unity event raised when the game event is triggered.")]
+            public UnityEvent onGameEventRaised;
+        }
     }
 }
