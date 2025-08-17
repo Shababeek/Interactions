1d4f5b7dd7998f696372d0c56efbd56c2a4a9894
ahmadabobakr <ahmadabobakr@gmail.com>
2025-08-17 15:15:34 +0400

    ADded documentation and TODO to unity events

--- Interactions/Scripts/Core/Runtime/ScriptableSystem/Events/UnityEvents.cs
+++ Interactions/Scripts/Core/Runtime/ScriptableSystem/Events/UnityEvents.cs
@@ -4,15 +4,17 @@ using UnityEngine.Events;
 
 namespace Shababeek.Core
 {
-    [Serializable]
-    public class FloatUnityEvent : UnityEvent<float>{}
-    [Serializable]
-    public class Vector3UnityEvent : UnityEvent<Vector3>{}
-    [Serializable]
-    public class IntUnityEvent : UnityEvent<int>{}
-    [Serializable]
-    public class StringUnityEvent : UnityEvent<string> { }
-    [Serializable]
-    public class     Vector2UnityEvent : UnityEvent<Vector2>{}
+    /// Collection of typed Unity events for common data types.
 
+    [Serializable]
+    public class FloatUnityEvent : UnityEvent<float> { }
+    [Serializable]
+    public class Vector3UnityEvent : UnityEvent<Vector3> { }
+    [Serializable]
+    public class IntUnityEvent : UnityEvent<int> { }
+    [Serializable]
+    public class StringUnityEvent : UnityEvent<string> { }   
+    [Serializable]
+    public class Vector2UnityEvent : UnityEvent<Vector2> { }
+    
 }
